2d4390fa37562835f8da0667ca745c4e8b65328c
Dhruv Ramesh <dhruvramesh1@gmail.com>
2023-04-02 20:38:42 -0400

    Working topics and entries pages to peruse

---
+++ learning_logs/urls.py
@@ -0,0 +1,18 @@
+"""Defines URL patterns for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+        # Home page
+        path('', views.index, name = 'index'),
+
+        # Show all topics
+        path(r'^topics/$', views.topics, name = 'topics'),
+
+        # Detail page for a single topic
+        path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic')
+    ]
