49476c8934eaf6234ebdb3d0d596234a0373ff0b
Dhruv Ramesh <dhruvramesh1@gmail.com>
2023-04-04 08:42:49 -0400

    Added form to add new topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -14,5 +14,8 @@ urlpatterns = [
         path(r'^topics/$', views.topics, name = 'topics'),
 
         # Detail page for a single topic
-        path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic')
+        path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name = 'topic'),
+
+        # Page for adding a new topic
+        path(r'^new_topic/$', views.new_topic, name = 'new_topic'),
     ]
